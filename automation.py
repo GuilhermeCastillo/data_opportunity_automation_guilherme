14228280c04b55831587c157b3221e3d35843fd5
GuilhermeCastillo <guilherme.castilhot2c18@gmail.com>
2023-01-22 12:28:05 -0300

    save changes

--- automation.py
+++ automation.py
@@ -1,7 +1,8 @@
-from urllib import response
 import requests
 from bs4 import BeautifulSoup
 import os
+from pdf_reader import PDF
+import pandas as pd
 
 
 def get_pdf(base_url):
@@ -28,8 +29,34 @@ def write_file_local(urls, output_path="/home/guilherme/projects/data_opportunit
     return
 
 
-if __name__ == '__main__':
-    base_url = "https://www.gob.mx/conadesuca/documentos/dieproc-reportes-de-avance-de-produccion-ciclo-azucarero-2020-2021?state=published"
-    urls_pdf = get_pdf(base_url)
+def df_to_xlsx(df):
+    df.to_csv('reportes.csv', index=False)
+    df.to_excel('reportes.xlsx', sheet_name='Cuadro 7', index=False)
+    os.remove(os.getcwd()+"/reportes.csv")
+    return
 
-    write_file_local(urls_pdf[1:6])
+
+base_url = "https://www.gob.mx/conadesuca/documentos/dieproc-reportes-de-avance-de-produccion-ciclo-azucarero-2020-2021?state=published"
+urls_pdf = get_pdf(base_url)
+write_file_local(urls_pdf[1:6])
+dir_pdfs = os.getcwd()+'/temp'
+list_pdfs = os.listdir(dir_pdfs)
+
+list_df_final = []
+for reporte in list_pdfs:
+    pdf = PDF(dir_pdfs+"/"+reporte)
+    page = pdf.search_index_table_by_title()
+
+    list_df = []
+    for i in range(0, 6):
+        list_df.append(pdf.create_table(i, page))
+
+    df_final = pd.concat(list_df, ignore_index=True)
+    list_df_final.append(df_final)
+
+df_final = pd.concat(list_df_final, ignore_index=True)
+
+df_to_xlsx(df_final)
+
+if __name__ == '__main__':
+    ...
--- pdf_reader.py
+++ pdf_reader.py
@@ -1,42 +1,47 @@
 import pandas as pd
 import tabula
 import numpy as np
+import PyPDF2
+import re
 
 
 class PDF():
     def __init__(self, file_name):
-        self.table_list = tabula.read_pdf(file_name, pages="6")
+        self.file_name = file_name
 
-    def len_table_list(self):
-        print(len(self.table_list))
+    # Encotra o indíce uma mágina através do título
+    def search_index_table_by_title(self, title="Cuadro 7"):
+        read_pdf = PyPDF2.PdfFileReader(self.file_name)
+        # pega o numero de páginas
+        number_of_pages = read_pdf.getNumPages()
+        count = 0
 
-    def create_table1(self):
-        df = self.table_list[0]
+        while count < number_of_pages:
+            page = read_pdf.getPage(count)
+            page_content = page.extractText()
+
+            # faz a junção das linhas
+            parsed = ''.join(page_content)
+            # remove as quebras de linha
+            parsed = re.sub('n', '', parsed)
+
+            if title in parsed:
+                return str(count+1)
+            count += 1
+
+        return "index não encontrado"
+
+    def date_report(self):
+
+        return
+
+    # ler as tabelas de uma página do pdf, transcrevendo as tabelas para um dataframe
+    def create_table(self, index, page):
+        table_list = tabula.read_pdf(self.file_name, pages=page)
+        df = table_list[index]
         header = list(df.columns)
-        df = df.rename(columns={header[0]: 'Ingenio',
-                                header[1]: 'Superficie industrializada',
-                                header[2]: 'Caña molida',
-                                header[3]: 'Rendimiento de campo',
-                                header[4]: 'Producción de azúcar',
-                                header[5]: 'Rendimiento de fábrica',
-                                header[6]: 'Rendimiento agroindustrial',
-                                header[7]: 'Azúcar refinada',
-                                header[8]: 'Azúcar estándar',
-                                header[9]: 'Azúcar blanco especial',
-                                header[10]: 'Azúcar mascabado',
-                                header[11]: 'Azúcar con pol menor a 99.2'})
-
-        df = df.drop([0, 1])
-        df = df.where(pd.notnull(df), None)
-        df = df.replace(' ', '', regex=True)
-        df = df.reset_index(drop=True)
-
-        return df
-
-    def create_table(self, index):
-        df = self.table_list[index]
-        header = list(df.columns)
-        header[:] = [x for x in header if not x.startswith('Unnamed:')]
+        if index != 0:
+            header[:] = [x for x in header if not x.startswith('Unnamed:')]
 
         df = df.rename(columns={
             header[0]: 'Ingenio',
@@ -68,21 +73,27 @@ class PDF():
                                                     'Azúcar mascabado',
                                                     'Azúcar con pol menor a 99.2'
                                                     ])
-        df = pd.concat([df, df_header])
+
+        if index == 0:
+            df = df.drop([0, 1])
+
+        if index != 0:
+            df = pd.concat([df, df_header])
         # df = df.append(
         #     pd.Series(header, index=df.columns[:len(header)]), ignore_index=True)
         df = df.replace('-.1', '-', regex=True)
         df = df.where(pd.notnull(df), None)
         df = df.replace(' ', '', regex=True)
-        df = df.iloc[np.arange(-1, len(df)-1)]
+        if index != 0:
+            df = df.iloc[np.arange(-1, len(df)-1)]
         df = df.reset_index(drop=True)
 
-        return df
+        df_final = df.drop(['Rendimiento de campo', 'Producción de azúcar', 'Rendimiento de fábrica', 'Rendimiento agroindustrial',
+                           'Azúcar refinada', 'Azúcar estándar', 'Azúcar mascabado', 'Azúcar con pol menor a 99.2'], axis=1)
+
+        return df_final
 
 
 if __name__ == '__main__':
-    pdf1 = PDF("Reporte_36.pdf")
-    pdf1.len_table_list()
-
-    tb1 = pdf1.create_table1()
-    tb2 = pdf1.create_table(1)
+    teste = PDF("Reporte_36.pdf")
+    print(teste.create_table(1, 6))
